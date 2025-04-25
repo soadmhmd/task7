60d95438f78e0b57b2a42024ef1fb2010358f59a
soadmhmd <sadmhmd292@gmail.com>
2025-04-25 03:32:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int size;
+    cout<<"ENTER the array size:"<<endl;
+    cin>>size;
+    int *arr=new int[size];
+    for (int i=0; i<size; i++) {
+        arr[i]=(i+1);
+    }
+    cout<<"Array elements:";
+    for (int i=0; i<size; i++) {
+        cout<<arr[i]<<" ";
+    }
+    delete [] arr;
+    return 0;
+}
